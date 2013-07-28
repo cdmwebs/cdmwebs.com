76124a57a2b6e80b563cb3288abe68b26710b21f
Chris Moore <cdmwebs@gmail.com>
2013-07-27 23:18:16 -0400

    initial rails commit

---
+++ config/initializers/raven.rb
@@ -0,0 +1,5 @@
+require 'raven'
+
+Raven.configure do |config|
+  config.dsn = ENV.fetch('SENTRY_DSN')
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cdmwebs::Application.config.secret_key_base = 'c646fea69175946798c1b7cb93b19bb571aaac0fe3f1468569555ad44f46939bd43d9dd08f73e1fceadf70178c7233c73944cb018103509bd1788147d7988d60'
